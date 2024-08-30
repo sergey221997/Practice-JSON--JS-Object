bacb51296097bad349e5ca51dc7e7564871e872a
Sergey Doronin <sergeydoronin199722@gmail.com>
2024-08-30 14:12:22 +0500

    add js file

---
+++ object.js
@@ -0,0 +1,14 @@
+const user = {
+    name: 'Sergey',
+    age: 27,
+    contact: { 
+        email: "sergeydoronin199722",
+        phone: {
+            mobile: "+998911921079",
+            home: "+99712837696"
+        }    
+    }
+}
+
+console.log(user.name)
+console.log(user.age)
